7acffed23606ceea80b6791eb7c421af39a415ee
Andrew Charneski <acharneski@gmail.com>
2023-11-23 19:14:32 -0500

    Update OAuthPatreon.kt

--- src/main/kotlin/com/simiacryptus/skyenet/webui/servlet/OAuthPatreon.kt
+++ src/main/kotlin/com/simiacryptus/skyenet/webui/servlet/OAuthPatreon.kt
@@ -1,5 +1,6 @@
 package com.simiacryptus.skyenet.webui.servlet
 
+import com.patreon.PatreonAPI
 import com.simiacryptus.jopenai.util.JsonUtil
 import com.simiacryptus.skyenet.core.platform.ApplicationServices
 import com.simiacryptus.skyenet.core.platform.AuthenticationManager
@@ -51,24 +52,15 @@ open class OAuthPatreon(
         override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
             val code = req.getParameter("code")
             if (code != null) {
-                val bodyString =
-                    "code=$code&grant_type=authorization_code&client_id=${config.clientId}&client_secret=${config.clientSecret}&redirect_uri=$redirectUri"
-                log.info("""
-                    Finalizing login with code $code
-                    config.clientId = ${config.clientId}
-                    config.clientSecret = ...${config.clientSecret.takeLast(4)}
-                    redirectUri = $redirectUri
-                    bodyString = $bodyString
-                """.trimIndent())
                 val tokenResponse = Request.create(Method.POST, URI(patreonTokenUrl))
                     .bodyString(
-                        bodyString,
+                        "code=$code&grant_type=authorization_code&client_id=${config.clientId}&client_secret=${config.clientSecret}&redirect_uri=$redirectUri",
                         ContentType.APPLICATION_FORM_URLENCODED
                     ).execute().returnContent().asString()
                 val accessToken = extractAccessToken(tokenResponse) // Implement this function to parse the JSON response
                 val userInfoResponse = Request.create(Method.GET, URI(patreonUserUrl))
                     .addHeader("Authorization", "Bearer $accessToken")
-                    .execute().returnContent().asString() // <-- 400 Bad Request
+                    .execute().returnContent().asString()
                 val user = parseUserInfo(userInfoResponse)
                 ApplicationServices.authenticationManager.putUser(accessToken = accessToken, user = user)
                 log.info("User $user logged in with session $accessToken")
@@ -93,19 +85,12 @@ open class OAuthPatreon(
     )
 
     data class PatreonUserResponse(
-        val data: UserData
-    )
-
-    data class UserData(
-        val id: String,
-        val attributes: UserAttributes
-    )
-
-    data class UserAttributes(
-        val email: String,
-        val full_name: String,
-        val image_url: String
-        // ... add other user attributes if necessary
+        val access_token: String,
+        val expires_in: Long,
+        val token_type: String,
+        val scope: String,
+        val refresh_token: String,
+        val version: String,
     )
 
     private fun extractAccessToken(tokenResponse: String?): String {
@@ -118,13 +103,14 @@ open class OAuthPatreon(
     private fun parseUserInfo(userInfoResponse: String?): User {
         if (userInfoResponse == null) throw RuntimeException("User info response is null")
         log.info("User info response: $userInfoResponse")
-        val userData = JsonUtil.fromJson<PatreonUserResponse>(userInfoResponse, PatreonUserResponse::class.java).data
-        val attributes = userData.attributes
+        val accessData = JsonUtil.fromJson<PatreonUserResponse>(userInfoResponse, PatreonUserResponse::class.java)
+        val fetchUser = PatreonAPI(accessData.access_token).fetchUser()
+        val user = fetchUser.get()
+        log.info("User data: ${JsonUtil.toJson(user)}")
         return User(
-            email = attributes.email,
-            id = userData.id,
-            name = attributes.full_name,
-            picture = attributes.image_url
+            email = user.email,
+            name = user.fullName,
+            picture = user.thumbUrl
         )
     }
 
