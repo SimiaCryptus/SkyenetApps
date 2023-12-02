f2f2b8603f1da269bb0491eb06a1d77e5d64b364
Andrew Charneski <acharneski@gmail.com>
2023-12-01 22:33:50 -0500

    Update DatabaseServices.kt

--- src/main/kotlin/com/simiacryptus/skyenet/platform/DatabaseServices.kt
+++ src/main/kotlin/com/simiacryptus/skyenet/platform/DatabaseServices.kt
@@ -1,7 +1,7 @@
-
 import com.simiacryptus.jopenai.ApiModel
 import com.simiacryptus.jopenai.models.OpenAIModel
 import com.simiacryptus.skyenet.core.platform.*
+import com.simiacryptus.skyenet.core.platform.file.DataStorage
 import java.io.File
 import java.sql.Connection
 import java.sql.DriverManager
@@ -26,32 +26,39 @@ open class DatabaseServices(
     getConnection().use { connection ->
       connection.autoCommit = false
       try {
-        executeSql(connection, """
+        executeSql(
+          connection, """
                     CREATE TABLE users (
                         id VARCHAR(255) PRIMARY KEY,
                         email VARCHAR(255) NOT NULL UNIQUE,
                         name VARCHAR(255),
                         picture VARCHAR(255)
                     );
-                """.trimIndent())
+                """.trimIndent()
+        )
 
-        executeSql(connection, """
+        executeSql(
+          connection, """
                     CREATE TABLE sessions (
                         session_id VARCHAR(255) PRIMARY KEY,
                         user_id VARCHAR(255),
                         FOREIGN KEY (user_id) REFERENCES users(id)
                     );
-                """.trimIndent())
+                """.trimIndent()
+        )
 
-        executeSql(connection, """
+        executeSql(
+          connection, """
                     CREATE TABLE user_settings (
                         user_id VARCHAR(255) PRIMARY KEY,
                         api_key VARCHAR(255),
                         FOREIGN KEY (user_id) REFERENCES users(id)
                     );
-                """.trimIndent())
+                """.trimIndent()
+        )
 
-        executeSql(connection, """
+        executeSql(
+          connection, """
                     CREATE TABLE usage (
                         usage_id INT AUTO_INCREMENT PRIMARY KEY,
                         session_id VARCHAR(255),
@@ -62,18 +69,22 @@ open class DatabaseServices(
                         FOREIGN KEY (session_id) REFERENCES sessions(session_id),
                         FOREIGN KEY (user_id) REFERENCES users(id)
                     );
-                """.trimIndent())
+                """.trimIndent()
+        )
 
-        executeSql(connection, """
+        executeSql(
+          connection, """
                     CREATE TABLE messages (
                         message_id INT AUTO_INCREMENT PRIMARY KEY,
                         session_id VARCHAR(255),
                         message_text TEXT,
                         FOREIGN KEY (session_id) REFERENCES sessions(session_id)
                     );
-                """.trimIndent())
+                """.trimIndent()
+        )
 
-        executeSql(connection, """
+        executeSql(
+          connection, """
                     CREATE TABLE authorizations (
                         authorization_id INT AUTO_INCREMENT PRIMARY KEY,
                         user_id VARCHAR(255),
@@ -81,7 +92,8 @@ open class DatabaseServices(
                         operation_type ENUM('Read', 'Write', 'Share', 'Execute', 'Delete', 'Admin', 'GlobalKey'),
                         FOREIGN KEY (user_id) REFERENCES users(id)
                     );
-                """.trimIndent())
+                """.trimIndent()
+        )
 
         connection.commit()
       } catch (e: Exception) {
@@ -94,88 +106,215 @@ open class DatabaseServices(
 
   val usageManager: UsageInterface = object : UsageInterface {
     override fun incrementUsage(session: Session, user: User?, model: OpenAIModel, tokens: ApiModel.Usage) {
-      TODO("Not yet implemented")
+      getConnection().use { connection ->
+        connection.autoCommit = false
+        try {
+          executeSql(
+            connection, """
+                    INSERT INTO usage (session_id, user_id, model, input_tokens, output_tokens)
+                    VALUES (?, ?, ?, ?, ?);
+                """.trimIndent()
+          )
+          connection.commit()
+        } catch (e: Exception) {
+          connection.rollback()
+          throw e
+        }
+      }
     }
 
     override fun getUserUsageSummary(user: User): Map<OpenAIModel, ApiModel.Usage> {
-      TODO("Not yet implemented")
+      getConnection().use { connection ->
+        connection.autoCommit = false
+        try {
+          executeSql(
+            connection, """
+                    SELECT model, SUM(input_tokens) AS input_tokens, SUM(output_tokens) AS output_tokens
+                    FROM usage
+                    WHERE user_id = ?
+                    GROUP BY model;
+                """.trimIndent()
+          )
+          connection.commit()
+        } catch (e: Exception) {
+          connection.rollback()
+          throw e
+        }
+      }
+      return mapOf()
     }
 
     override fun getSessionUsageSummary(session: Session): Map<OpenAIModel, ApiModel.Usage> {
-      TODO("Not yet implemented")
+      getConnection().use { connection ->
+        connection.autoCommit = false
+        try {
+          executeSql(
+            connection, """
+                    SELECT model, SUM(input_tokens) AS input_tokens, SUM(output_tokens) AS output_tokens
+                    FROM usage
+                    WHERE session_id = ?
+                    GROUP BY model;
+                """.trimIndent()
+          )
+          connection.commit()
+        } catch (e: Exception) {
+          connection.rollback()
+          throw e
+        }
+      }
+      return mapOf()
     }
   }
   val userSettingsManager: UserSettingsInterface = object : UserSettingsInterface {
     override fun getUserSettings(user: User): UserSettingsInterface.UserSettings {
-      TODO("Not yet implemented")
+      getConnection().use { connection ->
+        connection.autoCommit = false
+        try {
+          executeSql(
+            connection, """
+                    SELECT api_key
+                    FROM user_settings
+                    WHERE user_id = ?;
+                """.trimIndent()
+          )
+          connection.commit()
+        } catch (e: Exception) {
+          connection.rollback()
+          throw e
+        }
+      }
+      return UserSettingsInterface.UserSettings()
     }
 
     override fun updateUserSettings(user: User, settings: UserSettingsInterface.UserSettings) {
-      TODO("Not yet implemented")
+      getConnection().use { connection ->
+        connection.autoCommit = false
+        try {
+          executeSql(
+            connection, """
+                    INSERT INTO user_settings (user_id, api_key)
+                    VALUES (?, ?)
+                    ON DUPLICATE KEY UPDATE api_key = ?;
+                """.trimIndent()
+          )
+          connection.commit()
+        } catch (e: Exception) {
+          connection.rollback()
+          throw e
+        }
+      }
     }
   }
   val authenticationManager: AuthenticationInterface = object : AuthenticationInterface {
     override fun getUser(accessToken: String?): User? {
-      TODO("Not yet implemented")
+      getConnection().use { connection ->
+        connection.autoCommit = false
+        try {
+          executeSql(
+            connection, """
+                    SELECT id, email, name, picture
+                    FROM users
+                    WHERE id = ?;
+                """.trimIndent()
+          )
+          connection.commit()
+        } catch (e: Exception) {
+          connection.rollback()
+          throw e
+        }
+      }
+      return null
     }
 
     override fun putUser(accessToken: String, user: User): User {
-      TODO("Not yet implemented")
+      getConnection().use { connection ->
+        connection.autoCommit = false
+        try {
+          executeSql(
+            connection, """
+                    INSERT INTO users (id, email, name, picture)
+                    VALUES (?, ?, ?, ?)
+                    ON DUPLICATE KEY UPDATE email = ?, name = ?, picture = ?;
+                """.trimIndent()
+          )
+          connection.commit()
+        } catch (e: Exception) {
+          connection.rollback()
+          throw e
+        }
+      }
+      return user
     }
 
     override fun logout(accessToken: String, user: User) {
-      TODO("Not yet implemented")
+      getConnection().use { connection ->
+        connection.autoCommit = false
+        try {
+          executeSql(
+            connection, """
+                    DELETE FROM users
+                    WHERE id = ?;
+                """.trimIndent()
+          )
+          connection.commit()
+        } catch (e: Exception) {
+          connection.rollback()
+          throw e
+        }
+      }
     }
   }
-  val dataStorageFactory: (File) -> StorageInterface = { file -> object : StorageInterface {
-    override fun <T> getJson(user: User?, session: Session, filename: String, clazz: Class<T>): T? {
-      TODO("Not yet implemented")
-    }
+  val dataStorageFactory: (File) -> StorageInterface = { storageRoot ->
+    object : DataStorage(storageRoot) {
+      override fun <T> getJson(user: User?, session: Session, filename: String, clazz: Class<T>): T? {
+        return super.getJson(user, session, filename, clazz)
+      }
 
-    override fun getMessages(user: User?, session: Session): LinkedHashMap<String, String> {
-      TODO("Not yet implemented")
-    }
+      override fun getMessages(user: User?, session: Session): LinkedHashMap<String, String> {
+        return super.getMessages(user, session)
+      }
 
-    override fun getSessionDir(user: User?, session: Session): File {
-      TODO("Not yet implemented")
-    }
+      override fun getSessionDir(user: User?, session: Session): File {
+        return super.getSessionDir(user, session)
+      }
 
-    override fun getSessionName(user: User?, session: Session): String {
-      TODO("Not yet implemented")
-    }
+      override fun getSessionName(user: User?, session: Session): String {
+        return super.getSessionName(user, session)
+      }
 
-    override fun getSessionTime(user: User?, session: Session): Date? {
-      TODO("Not yet implemented")
-    }
+      override fun getSessionTime(user: User?, session: Session): Date? {
+        return super.getSessionTime(user, session)
+      }
 
-    override fun listSessions(user: User?): List<Session> {
-      TODO("Not yet implemented")
-    }
+      override fun listSessions(user: User?): List<Session> {
+        return super.listSessions(user)
+      }
 
-    override fun listSessions(dir: File): List<String> {
-      TODO("Not yet implemented")
-    }
+      override fun listSessions(dir: File): List<String> {
+        return super.listSessions(dir)
+      }
 
-    override fun <T : Any> setJson(user: User?, session: Session, filename: String, settings: T): T {
-      TODO("Not yet implemented")
-    }
+      override fun <T : Any> setJson(user: User?, session: Session, filename: String, settings: T): T {
+        return super.setJson(user, session, filename, settings)
+      }
 
-    override fun updateMessage(user: User?, session: Session, messageId: String, value: String) {
-      TODO("Not yet implemented")
-    }
+      override fun updateMessage(user: User?, session: Session, messageId: String, value: String) {
+        return super.updateMessage(user, session, messageId, value)
+      }
 
-    override fun userRoot(user: User?): File {
-      TODO("Not yet implemented")
-    }
+      override fun userRoot(user: User?): File {
+        return super.userRoot(user)
+      }
 
-    override fun deleteSession(user: User?, session: Session) {
-      TODO("Not yet implemented")
+      override fun deleteSession(user: User?, session: Session) {
+        return super.deleteSession(user, session)
+      }
     }
-  } }
+  }
 
-  companion object  {
+  companion object {
     @JvmStatic
-    fun main() {
+    fun main(args: Array<String>) {
       val databaseServices = DatabaseServices()
       databaseServices.initializeSchema()
       println("Database schema initialized successfully.")
