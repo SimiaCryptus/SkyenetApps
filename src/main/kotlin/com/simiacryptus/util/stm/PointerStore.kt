38bc32f7a407800adb6657d23e8d75d3237ae5b0
Andrew Charneski <acharneski@gmail.com>
2023-12-30 08:01:43 -0500

    move out

--- src/main/kotlin/com/simiacryptus/util/stm/PointerStore.kt
+++
@@ -1,8 +0,0 @@
-package com.simiacryptus.util.stm
-
-interface PointerStore {
-  fun set(key: Int, value: Int)
-  fun set(vararg kv: Pair<Int, Int>)
-  fun get(key: Int): Int
-  fun newPointer(): Int
-}
