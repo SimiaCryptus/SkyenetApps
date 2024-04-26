a5993303edc5a52d81946b5432ce9919cf5ec972
Andrew Charneski <acharneski@gmail.com>
2024-04-26 09:59:34 -0400

    1.0.66

--- App/build.gradle.kts
+++ App/build.gradle.kts
@@ -35,7 +35,7 @@ java {
 }
 
 val jetty_version = "11.0.18"
-val skyenet_version = "1.0.65"
+val skyenet_version = "1.0.66"
 val scala_version = "2.13.8"
 val spark_version = "3.5.0"
 val jackson_version = "2.17.0"
